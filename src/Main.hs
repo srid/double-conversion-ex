0110d327c1068aa2bd09d082e972be4386a42874
Sridhar Ratnakumar <srid@srid.ca>
2024-03-07 20:38:57 -0500

    Introduce TH
    - https://github.com/tweag/rules_haskell/issues/1601
    - https://gitlab.haskell.org/ghc/ghc/-/issues/23138

--- src/Main.hs
+++ src/Main.hs
@@ -1,7 +1,14 @@
+{-# LANGUAGE TemplateHaskell #-}
+
 module Main where
 
 import Data.Double.Conversion.Text
 import Data.Text
+
+import Language.Haskell.TH
+
+x :: Text
+x = $(runQ [|pack "don't even have to call double-conversion"|])
 
 main :: IO ()
 main = do
